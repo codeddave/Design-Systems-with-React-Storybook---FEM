b7e7a43253b601cc71554883f4117cca23b7f46c
codeddave <drizzzydave@gmail.com>
2022-05-31 03:52:32 +0100

    building Secondary button

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+const primaryColor = "#2CA6A4";
 const Button = styled.button`
   border-radius: 2px;
   padding: 12px 24px;
@@ -10,7 +11,21 @@ const Button = styled.button`
 `;
 
 const PrimaryButton = styled(Button)`
-  background-color: red;
+  background-color: ${primaryColor};
+  border: none;
+  color: white;
+`;
+export const SecondaryButton = styled(Button)`
+  background: none;
+  border: 1px solid ${primaryColor};
+  color: ${primaryColor};
+  &:hover {
+    background-color: ${primaryColor};
+    color: white;
+  }
+`;
+const TertiaryButton = styled(Button)`
+  background-color: ${primaryColor};
   border: none;
   color: white;
 `;
