e77c4a732f7acdf747952e7f8665df5feb0c9103
codeddave <drizzzydave@gmail.com>
2022-06-11 21:25:01 +0100

    updated status colors

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,4 +1,4 @@
-import { blue, neutral } from "./colors";
+import { blue, neutral, red, yellow, green } from "./colors";
 import { primaryFont } from "./typography";
 
 export const defalutTheme = {
@@ -6,11 +6,21 @@ export const defalutTheme = {
   primaryColorHover: blue[200],
   primaryColorActive: blue[100],
   primaryColorDisabled: neutral[300],
-
   textColorOnPrimary: neutral[100], //white
   textColor: neutral[600], //dark grey
   textColorInverted: neutral[100],
   disabled: neutral[400],
   textOnDisabled: neutral[300],
   primaryFont,
+  status: {
+    warningColor: yellow[100],
+    warningColorHover: yellow[200],
+    warningColorActive: yellow[300],
+    errorColor: red[100],
+    errorColorHover: red[200],
+    errorColorActive: red[300],
+    successColor: green[100],
+    successColorHover: green[200],
+    successColorActive: green[300],
+  },
 };
